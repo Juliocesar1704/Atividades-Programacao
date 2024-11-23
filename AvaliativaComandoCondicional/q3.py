a6a734ca3fb5d1cc387d047c3ddd4384d8fd5786
David Douglas Santos Barbosa <d4v1d.soudouglas@gmail.com>
2024-11-22 21:17:13 -0300

    Update q3.py

--- AvaliativaComandoCondicional/q3.py
+++ AvaliativaComandoCondicional/q3.py
@@ -1,26 +1,28 @@
 try:
-
+    # Sorteia um número entre 1 e 100 esse é o primeiro procedimento 
+    # Importar a biblioteca para utilizarmos o randint
     import random
-
+    # A primeira tentativa saíra do palpite dito pelo canditado para saber se esta proximo ou longe do numero sortido
+    # O randint é pegar os dois parametros o menor 1 e o maior
     numero_sorteado = random.randint(1, 100)
     intervalo_min = 1
     intervalo_max = 100
-
+# Agora teremos outro parametro  que será o palpite do jogador 
     print('Tente até 4 vezes adivinhar o número sorteado entre 1 e 100, Boa sorte')
     print('Primeiro chute : O intervalo é de',  intervalo_min, 'a', intervalo_max)
-    
+# Apos seu palpite se ele for maior que as extremidades 1 ou 100 ele ocupara como intervalo maximo ou minimo
     palpite = int(input('Digite seu palpite: '))
     if palpite == numero_sorteado:
         print('Você acertou o número tenta agora na mega da virada',  numero_sorteado)
-
+# Se ele for menor que o numero sorteado será o intervalo minimo 
     elif palpite < numero_sorteado:
         intervalo_min =  palpite  
         print ( 'Você errou tente novamente um numero entre',  intervalo_min, 'a', intervalo_max) 
-
+# Se ele for maior que o numero sorteado será o intervalor máximo
     elif palpite > numero_sorteado:
         intervalo_max = palpite 
         print ('Você errou tenta novamente um numero entr',  intervalo_min, 'a', intervalo_max)
-
+# Em suma o código a partir de agora será repetido mais 3 vezes que será o limite de apostas
 
     palpite = int(input('Digite seu palpite: '))
     if palpite == numero_sorteado:
